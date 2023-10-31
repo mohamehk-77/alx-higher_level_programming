fd2bc33ed45758d5d8ba7d0056c6cf4627a812f1
Mohamed kamal <kmido9729@gmail.com>
2023-10-31 22:17:26 +0200

    python, if/else, looping

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alpha in range(ord('a'), ord('z')+1):
-    if alpha == ord('q') or alpha == ord('e'):
-       print("{}".format(chr(alpha)), end='')
+for alpha in range(ord('a'), ord('z') + 1):
+    if alpha not in (ord('q'), ord('e')):
+        print("{}".format(chr(alpha)), end='')
