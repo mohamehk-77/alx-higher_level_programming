a955ebb8891bb2326be5d5c1bc1d0422a13f8476
Mohamed kamal <kmido9729@gmail.com>
2023-11-03 01:41:07 +0200

    python, import-modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-import sys
-if __name__ == "__main":
-    args = sys.argv[1:]  # Get all command-line arguments except the script name itself
-    count = 0
-    for arg in args:
-        count += int(arg)
-    print(count)
+if __name__ == "__main__":
+    from sys import argv
+    len = len(argv)
+    if len == 1:
+        print(0)
+    elif len > 1:
+        total = 0
+        for i in range(1, len):
+            total += int(argv[i])
+        print("{:d}".format(total))
