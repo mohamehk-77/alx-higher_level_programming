ca983c8c8a0ba105dd39bbf5da127ca4aded1862
Mohamed kamal <kmido9729@gmail.com>
2023-11-08 22:57:00 +0200

    python dict

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    num_unique = set()
+    
+    for num in my_list:
+        if num not in num_unique:
+            num_unique.add(num)
+            result += num
+    return result
+        
