450762bc2cd3211fa1adfce7cc20e0f4f80cc82c
kamal <kmido9729@gmail.com>
2023-11-21 00:20:06 +0200

    exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     integer_find = 0
     try:
         for y in range(x):
-            print("{:d}".format(my_list[y]), end="")
-            integer_find += isinstance(my_list[y], int)
+            if type(my_list[y]) is int:
+                print("{:d}".format(my_list[y]), end="")
+                integer_find += 1
         print()
         return integer_find
     except IndexError:
