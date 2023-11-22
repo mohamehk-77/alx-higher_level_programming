35e95d236ed67e7e230253dbe9985a36b6c71a35
kamal <kmido9729@gmail.com>
2023-11-22 21:44:03 +0200

    oop

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script is used to create a new user in the database. The user's information will be stored in the 'users' table of the 1-Square
+"""
+
+
+class Square:
+    """
+    Class Square
+
+    Attributes:
+    - size: size of the side of the square
+    """
+
+    def __init__(self, size):
+        """Instantiation with size (no type/value verification)"""
+        self.__size = size
