d0230b551e6383501e7e296c36dc1d1d297b3b41
kamal <kmido9729@gmail.com>
2023-11-22 02:57:47 +0200

    oop

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 """
-Module 0-squer
-Defines class 
-
+Module 0-square
+Defines class Square
 """
 
 
 class Square:
-    """empty class defined"""
+    """Empty class"""
     pass
