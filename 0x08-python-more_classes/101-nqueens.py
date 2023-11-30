4c6c1e52456027379963d828990f6201634163ad
kamal <kmido9729@gmail.com>
2023-11-30 02:28:49 +0200

    more object

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+N Queens problem
+"""
+
+import sys
+
+
+def is_safe(board, row, col, n):
+    """
+    Check if it's safe to place a queen at board[row][col]
+    """
+    # Check this row on the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on the left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on the left side
+    for i, j in zip(range(row, n), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, col, n, solutions):
+    """
+    Solve the N Queens problem using backtracking
+    """
+    if col == n:
+        solutions.append([row_col for row_col in enumerate(board)])
+        return
+
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            solve_nqueens_util(board, col + 1, n, solutions)
+            board[i][col] = 0  # backtrack
+
+
+def print_solutions(solutions):
+    """
+    Print the solutions
+    """
+    for solution in solutions:
+        print(solution)
+
+
+def main():
+    """
+    Main function
+    """
+    if len(sys.argv) != 2:
+        print("Usage: {} N".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+
+    solve_nqueens_util(board, 0, n, solutions)
+    print_solutions(solutions)
+
+
+if __name__ == "__main__":
+    main()
