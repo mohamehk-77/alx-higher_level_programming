6ee8e2171d1a50c2315072561cfded013f9a5c5f
kamal <kmido9729@gmail.com>
2023-12-05 13:21:22 +0200

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,3 +10,4 @@ def inherits_from(obj, a_class):
     Check if obj is an instance of a class inherited from a_class.
     """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
+
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+BaseGeometry (based on 5-base_geometry.py)
+"""
+
+
+class BaseGeometry:
+
+    """
+    Public instance method: area() raises an Exception
+    """
+
+    def area(self):
+
+        raise Exception("area() is not implemented")
