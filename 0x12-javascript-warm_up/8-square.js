295881a0cc35bcbb3a1506653aaceadccaa2ad18
mohamed kamal <kmido9729@gmail.com>
2024-02-13 00:52:55 +0200

    first js code

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (isNaN(parseInt(process.argv[2], 10))) {
+    console.log('Missing size');
+} else {
+    for (let i = 0; i < parseInt(process.argv[2], 10); i++) {
+        console.log('X'.repeat(parseInt(process.argv[2], 10)));
+    }
+}
