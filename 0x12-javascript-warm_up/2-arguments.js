fc33985d357d3713a84910d1ca788bb13c946675
mohamed kamal <kmido9729@gmail.com>
2024-02-13 00:32:45 +0200

    first js code

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (!process.argv[2]) {
+    console.log('No arguments');
+}
+if (process.argv.length === 3) {
+    console.log('Argument found');
+} else if (process.argv.length > 3) {
+    console.log('Argument found');
+}
