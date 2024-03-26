eb92a9a77ed8ddd39a23a637a7bd28e7043a461e
Mohamed kamal <kmido9729@gmail.com>
2024-03-27 01:04:43 +0200

    api...js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// a script that display the status code of a GET request.
+// The first argument is the URL to request (GET)
+// The status code must be printed like this: code: <status code>
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('code:', response.statusCode);
+    }
+});
