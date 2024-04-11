a33144349e02545e1a40992962d6aaa082590178
mohamed kamal <kmido9729@gmail.com>
2024-04-11 21:18:13 +0200

    api is fun

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        return data['data']['subscribers']
+    else:
+        return 0
